51d614a675d4824e79bcf12cfb475878406def64
Analice Leite <analice.leite12@gmail.com>
2024-07-24 11:18:02 -0300

    feature: redux configurado

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,35 +1,17 @@
+import { useSelector } from 'react-redux'
+import { RootReducer } from '../../store'
+
 import Tarefa from '../../components/Tarefa'
 import { Container } from './styles'
 
-const tarefas = [
-  {
-    titulo: 'Estudar TypeScript',
-    descricao: 'Ver a aula 3 da EBAC',
-    prioridade: 'importante',
-    status: 'pendente'
-  },
-  {
-    titulo: 'Construir meu portfólio',
-    descricao: 'Ver anotações para insights',
-    prioridade: 'urgente',
-    status: 'pendente'
-  },
-  {
-    titulo: 'Ir para academia',
-    descricao: 'Fazer treino A',
-    prioridade: 'importante',
-    status: 'pendente'
-  }
-]
-
 const ListaDeTarefas = () => {
-  // Aqui você pode implementar a lógica para contar as tarefas com categorias específicas
-  const countCategoria = '' // Substitua com a lógica necessária
-  const countTermo = '' // Substitua com a lógica necessária
+  const { tarefas } = useSelector((state: RootReducer) => state)
 
   return (
     <Container>
-      <p>{`${countCategoria} tarefas marcadas como: "categoria" "${countTermo}"`}</p>
+      <p>
+        $ 2 tarefas marcadas como: &quot;categoria&ldquo; &quot;termo&ldquo;
+      </p>
       <ul>
         {tarefas.map((t) => (
           <li key={t.titulo}>
